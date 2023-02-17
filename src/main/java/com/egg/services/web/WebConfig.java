ef106bdbabba58071bde7d9cb1624f8f5020261a
Agustin <agsp3rs3o@gmail.com>
2023-02-17 11:21:51 -0300

    Updating WebConfig.java

--- src/main/java/com/egg/services/web/WebConfig.java
+++ src/main/java/com/egg/services/web/WebConfig.java
@@ -1,7 +1,6 @@
 package com.egg.services.web;
 
 import java.util.Locale;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
